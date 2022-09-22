9a3e37cc092944e880bb972cc6ab78f35ad373f0
HenryHitak <gmlxkr920901@gmail.com>
2022-09-22 13:39:37 -0700

    logout complete

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -5,30 +5,9 @@ import loginService from '../services/loginService';
 import {FloatingLabel, Form} from 'react-bootstrap';
 import style from '../style/mytemplate.module.css';
 function Logout(props){   
-    const passInput = useRef();
-    const [logFlag,setLogin] = useState(false);
-    const [Ip,setIp] = useState();
-    const [err,setErr] = useState();
+    props.LogoutFun('');
     const navigate = useNavigate();
-    const login = (event) =>{
-        event.preventDefault();
-        
-        const formData = new FormData(event.target);
-        formData.append('gip',Ip);
-        loginService.login(formData)
-            .then(response=>{
-                setLogin(true);
-                props.loginFun(response.data);
-                sessionStorage.setItem("sid",response.data.sid);
-                setErr(null);
-                navigate('/');
-            })
-            .catch(err=>{
-                setErr(err.response.data);
-            });
-    }
-    useEffect(()=>{
-        globalIP.getIP().then(data=>{setIp(data)});
-    },[]);
+    sessionStorage.removeItem("sid")
+    navigate('/login');
 }
 export default Logout;
