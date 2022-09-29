46356f954715d28f2bddc1a27b21e6546f38e3c6
HenryHitak <gmlxkr920901@gmail.com>
2022-09-29 15:19:05 -0700

    updated

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -3,8 +3,4 @@ import axios from "axios";
 export default axios.create({
     baseURL:"http://localhost/fproj/php/"
 
-import axios from "axios";
-export default axios.create({
-    baseURL:"http://localhost/finalproj/php/"
-
 })
